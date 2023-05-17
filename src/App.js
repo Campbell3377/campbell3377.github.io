26764433114168c0fe192349789812ca05e92e7a
Sean Campbell <seancampbell3377@gmail.com>
2023-05-17 11:54:57 -0700

    Test

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+// import "./index.css";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
